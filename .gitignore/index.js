581f613a46fe9cf506e21e798bdfe883d02eb254
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2019-05-19 18:35:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,3 +10,23 @@ client.on("guildMemberAdd", member =>{
     if(!channel) return;
     channel.send(`Bienvenue **${member}**, sur **${member.guild.name}**, veuillez lire les règles https://thumbs.gfycat.com/GrimMealyBuckeyebutterfly-size_restricted.gif`)
 })
+
+Client.on('message', message => {
+
+    if (message.content.startsWith(prefix + "sondage")) {
+        let args = message.content.split(" ").slice(1);
+        let thingToEcho = args.join(" ")
+        var embed = new Discord.RichEmbed()
+            .setDescription("Sondage")
+            .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+            .setColor("0xB40404")
+            .setTimestamp()
+        message.guild.channels.find("name", "sondage-📊").sendEmbed(embed)
+        .then(function (message) {
+            message.react("✅")
+            message.react("❌")
+        }).catch(function() {
+        });
+        }else{
+            return message.reply("Tu n'as pas la permission.")
+}}})
