e937030f4b5d502e569ba60d0099db8748b0252c
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2019-05-25 21:14:18 +0200

    Create queue.js

---
+++ .gitignore/commands/queue.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'queue',
+	description: 'Queue command.',
+	cooldown: 5,
+	execute(message) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) return message.channel.send('Il n'y a rien qui joue.');
+		return message.channel.send(`
+__**File d'attente de la chanson:**__
+${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
+**Lecture en cours:** ${serverQueue.songs[0].title}
+		`);
+	}
+};
