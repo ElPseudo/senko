2fa3ea963c69dc7eda0551356fe91c67b09933e9
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2019-05-25 19:27:00 +0200

    Add files via upload

---
+++ addrole.js
@@ -0,0 +1,24 @@
+const Discord = require(`discord.js`);
+
+module.exports.run = async(bot, message, args) => {
+
+    if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) {
+        return message.channel.send(`Tu n'as pas la permission`);
+    }
+
+    if(!message.guild.member(bot.user).hasPermission("MANAGE_ROLES")) {
+        return message.channel.send(`Je n'ai pas la permission !`);
+    }
+
+    let membre = message.guild.member(message.author);
+
+    let role = message.guild.roles.find(x => x.name === "Membre");
+
+    membre.addRole(role).catch(console.error);
+    message.channel.send(`${membre} a maintenant le rôle ${role.name}`);
+
+};
+
+module.exports.help = {
+    name: "addrole"
+}
