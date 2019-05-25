c958a0c4229e3626ba8995580ecb71080d2f27af
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2019-05-25 21:47:59 +0200

    Update skip.js

--- .gitignore/commands/skip.js
+++ .gitignore/commands/skip.js
@@ -6,7 +6,7 @@ module.exports = {
 		const { voiceChannel } = message.member;
 		if (!voiceChannel) return message.channel.send('Je suis désolé mais vous devez être sur un canal vocal pour jouer de la musique!');
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('Il n'y a rien que je puisse sauter pour vous.');
+		if (!serverQueue) return message.channel.send(`Il n'y a rien que je puisse sauter pour vous.`);
 		serverQueue.connection.dispatcher.end('La commande de saut a été utilisée!');
 	}
 };
