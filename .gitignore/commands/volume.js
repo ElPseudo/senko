0238e24657ebc157b06a5c96f802b2856b40c566
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2019-05-25 21:16:57 +0200

    Create volume.js

---
+++ .gitignore/commands/volume.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'volume',
+	description: 'Volume command.',
+	cooldown: 5,
+	execute(message, args) {
+		const { voiceChannel } = message.member;
+		if (!voiceChannel) return message.channel.send('Je suis désolé mais vous devez être sur un canal vocal pour jouer de la musique!');
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) return message.channel.send('Il n'y a rien qui joue.');
+		if (!args[0]) return message.channel.send(`Le volume actuel est: **${serverQueue.volume}**`);
+		serverQueue.volume = args[0]; // eslint-disable-line
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
+		return message.channel.send(`Je règle le volume à: **${args[0]}**`);
+	}
+};
