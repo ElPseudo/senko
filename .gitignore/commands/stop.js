32aac4019109a389f213b8ee8482936a580a13d1
ElPseudo <46242061+ElPseudo@users.noreply.github.com>
2019-05-25 21:48:37 +0200

    Update stop.js

--- .gitignore/commands/stop.js
+++ .gitignore/commands/stop.js
@@ -6,8 +6,8 @@ module.exports = {
 		const { voiceChannel } = message.member;
 		if (!voiceChannel) return message.channel.send('Je suis désolé mais vous devez être sur un canal vocal pour jouer de la musique!');
 		const serverQueue = message.client.queue.get(message.guild.id);
-		if (!serverQueue) return message.channel.send('Il n'y a rien que je puisse arrêter de jouer pour vous.');
+		if (!serverQueue) return message.channel.send(`Il n'y a rien que je puisse arrêter de jouer pour vous.`);
 		serverQueue.songs = [];
-		serverQueue.connection.dispatcher.end('La commande d'arrêt a été utilisée!');
+		serverQueue.connection.dispatcher.end(`La commande d'arrêt a été utilisée!`);
 	}
 };
